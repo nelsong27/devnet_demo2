e2c89c51ecad31686ad48d5dc0b18c83740e1383
nelsong27 <nelson.gomez@puppet.com>
2021-09-20 09:27:00 -0400

    Create dns_ntp.pp

---
+++ manifests/dns_ntp.pp
@@ -0,0 +1,20 @@
+class devnet_demo2::dns_ntp {
+
+network_dns { 'default':
+  ensure  => 'present',
+  servers => ['8.8.8.8', '8.8.4.4'],
+  search  => ['puppet.vm'],
+}
+ios_config { 'nameserver1':
+       command => 'ntp server 1.1.1.1',
+       idempotent_regex => 'ntp server 1.1.1.1'
+       }
+ios_config { 'nameserver2':
+       command => 'ntp server 1.1.1.2',
+       idempotent_regex => 'ntp server 1.1.1.2'
+     }
+ios_config { 'nameserver3':
+       command => 'ntp server 1.1.1.3',
+       idempotent_regex => 'ntp server 1.1.1.3'
+     }
+}
